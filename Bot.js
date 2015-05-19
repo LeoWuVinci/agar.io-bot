2ae3d7aec8f9743e30fa3b8be1814f62d91932f1
Leo W <heyitsleo@gmail.com>
2015-05-19 04:53:13 -0700

    some tweaks to the fitness calculation

--- Bot.js
+++ Bot.js
@@ -9,14 +9,15 @@ Coordinate.prototype={
 	fitness:0
 }
 
+
+//Map is 11150x11150
 function calcFitness(myOrganism,organism){
-	var sizeDiff=myOrganism.size-organism.size,
-		distance=Math.pow(Math.pow(myOrganism.x-organism.x,2)+Math.pow(myOrganism.y-organism.y,2),.5),
-		fitness=0
+	var sizeDiff=Math.abs(myOrganism.size-organism.size)/2000,
+		distance=(Math.pow(Math.pow(myOrganism.x-organism.x,2)+Math.pow(myOrganism.y-organism.y,2),.5)-myOrganism.size-organism.size)/1000,
+		midDistance=(Math.pow(Math.pow(5500-organism.x,2)+Math.pow(5500-organism.y,2),.5))/5500,
+		twiceSizeDiff=Math.abs(organism.size-myOrganism.size*2)/2000
 
-	fitness-=Math.abs(sizeDiff)
-
-	return fitness-distance
+	return -sizeDiff*2-distance*2-midDistance-twiceSizeDiff*3
 }
 
 var Bot=function(name,move){
@@ -47,23 +48,28 @@ Bot.prototype={
 
 			if (organism.name!=myOrganism.name){
 				if (organism.isVirus&&organism.size<myOrganism.size
-							||organism.size>myOrganism.size
+							||organism.size*.8>myOrganism.size
 				){
 					coordinate=new Coordinate(
 							myOrganism.x+myOrganism.x-organism.x,
 							myOrganism.y+myOrganism.y-organism.y,
 							calcFitness(myOrganism,organism))
+					coordinate.organism=organism
 				}else if (!organism.isVirus
-						&&organism.size<myOrganism.size){
+						&&organism.size<myOrganism.size*.8){
 					coordinate=new Coordinate(
 							organism.x,
 							organism.y,
 							calcFitness(myOrganism,organism))
+					coordinate.organism=organism
 				}
 			}
 
 			if(!bestCoordinate||bestCoordinate.fitness<coordinate.fitness){
 				bestCoordinate=coordinate
+				if(organism.name&&coordinate){
+					console.log(organism.name,Math.round(coordinate.fitness))
+				}
 			}
 		}
 
