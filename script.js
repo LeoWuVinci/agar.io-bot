a684331613defcfdd77c80e4b62339cfb3627e91
Leo W <heyitsleo@gmail.com>
2015-05-20 09:00:28 -0700

    cleaned up code that disables original js code and uses the modified version

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
+$('#region')
+	.after($('#region').removeAttr('onchange').clone().change(function(e){
+		setRegion($('#region').val());$('.region-message').hide();$('.region-message.'+$('#region').val()).show();$('.btn-needs-server').prop('disabled', false);	
+	})).remove()
 
-$('#region').after('<select id="region" class="form-control" required><option selected disabled value=""> -- Select a Region -- </option><option value="US-Fremont">US West</option></select>').remove()
-
-document.getElementById('region').onchange=function(e){
-	setRegion($('#region').val());$('.region-message').hide();$('.region-message.'+$('#region').val()).show();$('.btn-needs-server').prop('disabled', false);	
-}
-
-$('#playBtn').after('<button disabled type="submit" id="playBtn" class="btn btn-play btn-primary btn-needs-server">Play</button>').remove()
-
-document.getElementById('playBtn').onclick=function(e){
-	setNick(document.getElementById('nick').value); return false;
-}
+$('#playBtn')
+	.after($('#playBtn').removeAttr('onclick').clone().click(function(e){
+		setNick(document.getElementById('nick').value); return false;
+	})).remove()
